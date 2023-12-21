e22851d34832c3d994757860b2d5a7df1171855d
ybouali <yassine.bouali.bo@gmail.com>
2023-12-21 18:55:24 +0100

    Start working on the devOps part

---
+++ Makefile
@@ -0,0 +1,8 @@
+all:
+	docker-compose -f docker-compose.yml up --build -d
+
+clean:
+	docker-compose -f docker-compose.yml down
+
+fclean: clean
+	docker system prune -a -f
---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='flex items-center justify-center min-h-screen' >
+      <button className='border-2 border-gray-400 text-lg text-white bg-orange-500 rounded-xl p-2' >
+        LOG IN INTRA 42
+      </button>
+    </div>
+  );
+}
+
+export default App;
