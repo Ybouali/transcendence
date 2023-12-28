d6adcbd7ff315e0e5e16ec29bad57e0bef98305e
ybouali <yassine.bouali.bo@gmail.com>
2023-12-28 16:13:38 +0100

    swagger added

---
+++ back-end/src/auth/dto/AuthType.dto.ts
@@ -0,0 +1,12 @@
+import { IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger'
+
+export class AuthDto {
+
+    @ApiProperty({
+        description: 'this code u can get it from the intra 24 api'
+    })
+
+    @IsNotEmpty()
+    code: string;
+}
--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Obach bghiti ngolhalik')
+    .setDescription('had l project naaaaaaaaddiiiiiii')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   const port = process.env.PORT || 3333;
 
